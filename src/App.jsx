a95fe74015ba5360331e1cff6e094389da8c8c10
Federico-Baez <baezfede95@gmail.com>
2023-04-16 22:21:11 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import WeatherComponent from './components/containers/WeatherContainer'
+
+function App() {
+
+  return (
+    <div className="App">
+      <WeatherComponent />
+    </div>
+  )
+}
+
+export default App
